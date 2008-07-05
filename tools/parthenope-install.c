4cb4ccf776fceb70da81927794e4eb8f87b39d23
Giuseppe Coviello <cjg@cruxppc.org>
2008-07-05 14:15:17 +0000

    added copyright to parthenope-install.c
    --HG--
    extra : convert_revision : 6261e893a0167ff43a12b1c68841489b9989f5cd

--- tools/parthenope-install.c
+++ tools/parthenope-install.c
@@ -1,3 +1,25 @@
+/* parthenope-install.c */
+
+/* <project_name> -- <project_description>
+ *
+ * Copyright (C) 2006 - 2007
+ *     Giuseppe Coviello <cjg@cruxppc.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -316,7 +338,6 @@ int main(void)
 	install_slb(dev, rdb, slb, slb_size);
 	calculate_checksum(rdb);
 	write_block(dev, rdb, rdb_block);
-	printf("BootBlockList: %u\n", rdb->rdb_BootBlockList);
 	free(rdb);
 	fclose(dev);
 
