2b54c6c3b3ef7ca652445571af42a5b53ebcf32a
Giuseppe Coviello <giuseppe.coviello@uniparthenope.it>
2010-10-09 12:37:01 +0200

    added global header file

---
+++ src/parthenope.h
@@ -0,0 +1,8 @@
+char __attribute__ ((__used__)) * version =
+    "\0$VER: Parthenope 0." VERSION " (" DATE ") "
+    "Copyright (C) 2008 Giuseppe Coviello. "
+    "This is free software.  You may redistribute copies of it under the "
+    "terms of the GNU General Public License "
+    "<http://www.gnu.org/licenses/gpl.html>. There is NO WARRANTY, "
+    "to the extent permitted by law.";
+
